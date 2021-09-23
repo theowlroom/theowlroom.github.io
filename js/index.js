2f24bc7856bddaa09224c1d56a3064caed42ed46
deleiteEmpanadas <deleite.empanadas2012@gmail.com>
2021-09-23 17:30:05 -0500

    first commit

---
+++ js/index.js
@@ -0,0 +1,60 @@
+window.sr = ScrollReveal();
+
+	sr.reveal('.logo', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-50px', //distancia
+		
+
+
+	}); 
+
+	
+
+	sr.reveal('.atrevete', {
+		duration: 1500, //tiempo
+		origin: 'right', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+	sr.reveal('.catalogo', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset: 1000
+
+	});
+
+	sr.reveal('.buttonsCatalogo', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+	sr.reveal('.precios', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset: 1000
+
+	});
+
+	sr.reveal('.piepagina', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		reset: 1000
+
+	}); 
+
+	sr.reveal('.logoItem', {
+		duration: 1500, //tiempo
+		origin: 'left', //origen
+		distance: '-100px', //distancia
+		reset: 1000
+
+	}); 
