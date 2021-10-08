cfbe6151e197b7111718e2eca83d4ce1bed658a7
kalaqrdevelopment <kalaqr980108@gmail.com>
2021-10-07 22:46:46 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -1,68 +1,123 @@
 window.sr = ScrollReveal();
 
-	sr.reveal('.logo', {
+	sr.reveal('.imagen1', {
 		duration: 1500, //tiempo
 		origin: 'bottom', //origen
 		distance: '-50px', //distancia
-		
+		reset:1000
+
+
+	}); 
+	sr.reveal('.menu', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-50px' //distancia
 
 
 	}); 
 
+
+	sr.reveal('.img1', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+
+	sr.reveal('.img2', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
+		
+
+	});
+
+	sr.reveal('.img3', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
+		
+
+	});
+
+	sr.reveal('.img4', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
+		
+
+	});
+
+	sr.reveal('.img5', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
+
+	}); 
+
+	sr.reveal('.img6', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
+
+
+	}); 
+
+	sr.reveal('.img7', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
 	
 
-	sr.reveal('.atrevete', {
+	}); 
+
+	sr.reveal('.img8', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
+	
+
+	}); 
+
+	sr.reveal('.img9', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
+	
+
+	}); 
+
+	sr.reveal('.img10', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
+	
+
+	}); 
+
+	sr.reveal('.img11', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px' //distancia
+	
+
+	}); 
+
+
+	sr.reveal('.piepagina', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	}); 
+
+
+	sr.reveal('.table-responsive', {
 		duration: 1500, //tiempo
 		origin: 'right', //origen
 		distance: '-100px', //distancia
 		
 
 	});
-
-	sr.reveal('.catalogo', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px', //distancia
-		reset: 1000
-
-	});
-
-	sr.reveal('.buttonsCatalogo', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px', //distancia
-		
-
-	});
-
-	sr.reveal('.precios', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px', //distancia
-		reset: 1000
-
-	});
-
-	sr.reveal('.piepagina', {
-		duration: 1500, //tiempo
-		origin: 'bottom', //origen
-		distance: '-100px', //distancia
-		reset: 1000
-
-	}); 
-
-	sr.reveal('.float', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px', //distancia
-		reset: 1000
-
-	}); 
-
-	sr.reveal('.pricing-table', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px', //distancia
-	
-
-	}); 
