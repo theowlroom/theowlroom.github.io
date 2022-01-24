adb014a3fdc229ed52416faad9a2740b27e84376
deleitempanadas <deleite.empanadas2012@gmail.com>
2022-01-24 16:43:39 -0600

    first commit

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 window.sr = ScrollReveal();
 
-	sr.reveal('.imagen1', {
+	sr.reveal('.logo', {
 		duration: 1500, //tiempo
 		origin: 'bottom', //origen
 		distance: '-50px', //distancia
-		reset:1000
+		
 
 
 	}); 
@@ -17,7 +17,7 @@ window.sr = ScrollReveal();
 	}); 
 
 
-	sr.reveal('.img1', {
+	sr.reveal('.titulo', {
 		duration: 1500, //tiempo
 		origin: 'top', //origen
 		distance: '-100px', //distancia
@@ -25,84 +25,6 @@ window.sr = ScrollReveal();
 
 	});
 
-	sr.reveal('.img2', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-		
-
-	});
-
-	sr.reveal('.img3', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-		
-
-	});
-
-	sr.reveal('.img4', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-		
-
-	});
-
-	sr.reveal('.img5', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-
-	}); 
-
-	sr.reveal('.img6', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-
-
-	}); 
-
-	sr.reveal('.img7', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-	
-
-	}); 
-
-	sr.reveal('.img8', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-	
-
-	}); 
-
-	sr.reveal('.img9', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-	
-
-	}); 
-
-	sr.reveal('.img10', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-	
-
-	}); 
-
-	sr.reveal('.img11', {
-		duration: 1500, //tiempo
-		origin: 'top', //origen
-		distance: '-100px' //distancia
-	
-
-	}); 
 
 
 	sr.reveal('.piepagina', {
@@ -116,7 +38,7 @@ window.sr = ScrollReveal();
 
 	sr.reveal('.table-responsive', {
 		duration: 1500, //tiempo
-		origin: 'right', //origen
+		origin: 'bottom', //origen
 		distance: '-100px', //distancia
 		
 
